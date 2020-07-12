73b8c712a049318d6b896d7eddd8b2011abfaf50
Tatiana Samsonova <tatiana.samsonova228@gmail.com>
2020-07-12 12:30:48 +0300

    Configure the websocket endpoint and message broker

---
+++ src/main/java/com/learning/springwschatapp/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.learning.springwschatapp.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    //A registration of a websocket endpoint that the clients will use to connect to our websocket server
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //defines that the messages whose destination starts with “/app” should be routed to message-handling methods
+        registry.setApplicationDestinationPrefixes("/app");
+
+        //defines that the messages whose destination starts with “/topic” should be routed to the message broker
+        registry.enableSimpleBroker("/topic");
+
+    }
+}
