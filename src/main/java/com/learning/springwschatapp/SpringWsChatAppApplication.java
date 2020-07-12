8c8515753aced9008057f6433382d270802030db
Tatiana Samsonova <tatiana.samsonova228@gmail.com>
2020-07-12 12:09:16 +0300

    Initial commit

---
+++ src/main/java/com/learning/springwschatapp/SpringWsChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springwschatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWsChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWsChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/springwschatapp/SpringWsChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springwschatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWsChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
